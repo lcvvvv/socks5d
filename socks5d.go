6ff343cb9f2d140b9c6244566d7e624aa21cb7ac
kv2 <cheng.kv2@gmail.com>
2022-10-26 16:37:58 +0800

    update

--- socks5d.go
+++ socks5d.go
@@ -18,14 +18,14 @@ func init() {
 	case 1:
 		//	socks5://0.0.0.0:1080
 	case 2:
-		//	socks5://os.args[1]
+		//	socks5://listenAddr
 		listenAddr = os.Args[1]
 	case 3:
-		//	socks5://os.args[2]@os.args[1]
+		//	socks5://username@listenAddr
 		listenAddr = os.Args[1]
 		username = os.Args[2]
 	case 4:
-		//	socks5://os.args[2]:os.args[3]@os.args[1]
+		//	socks5:/username:password@listenAddr
 		listenAddr = os.Args[1]
 		username = os.Args[2]
 		password = os.Args[3]
@@ -49,7 +49,7 @@ func main() {
 		os.Exit(0)
 	}
 	// Create SOCKS5 proxy on localhost port 1080
-	fmt.Printf("listening socks5://%s", listenAddr)
+	fmt.Printf("listening socks5://%s\n", listenAddr)
 
 	if err := server.ListenAndServe("tcp", listenAddr); err != nil {
 		fmt.Println("[error]", err)
